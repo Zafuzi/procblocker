3cf3a2728c7483b8bcbfd17bf3ba36b07b18008c
Zachary Foutz <zachary@boomlearning.com>
2023-05-17 12:12:57 -0700

    * moved common functions to lib

---
+++ lib.mjs
@@ -0,0 +1,65 @@
+window.rootElement = document.querySelector("#root");
+window.cacheLoads = 0;
+
+const cachedModules = {};
+
+export function updateRouteInformation()
+{
+    window.route = window.location.pathname;
+    window.queryData = new URLSearchParams(window.location.search);
+    window.page = queryData.get('page') || "home";
+}
+
+export async function importModules()
+{
+    updateRouteInformation();
+    cachedModules.home = await import("/modules/home.mjs");
+    cachedModules.login = await import("/modules/login.mjs");
+}
+
+export function replaceLinks()
+{
+    // replace all <Link> elements with <a> elements and add event listeners
+    document.querySelectorAll("Link")?.forEach( link => {
+        const linkHref = link.getAttribute("href");
+        const anchor = document.createElement("a");
+        anchor.setAttribute("href", linkHref);
+        anchor.innerText = link.dataset.value;
+
+        anchor.addEventListener("click", async (event) =>
+        {
+            event.preventDefault();
+            const linkQueryData = new URLSearchParams(linkHref.split("?")[1]);
+            const linkPage = linkQueryData.get("page");
+
+            await navigate(linkPage);
+        });
+
+        link.replaceWith(anchor);
+    });
+}
+
+export async function navigate(moduleName)
+{
+    moduleName = moduleName || window.page || "home";
+    window.history.pushState({}, moduleName, `/?page=${moduleName}`);
+    document.adoptedStyleSheets = [];
+
+    await importModules();
+
+    if(!cachedModules[moduleName])
+    {
+        rootElement.innerHTML = `
+            <h2>404</h2>
+            <p>Page: ${page} was not found...</p>
+            <Link href="/?page=home" data-value="Go Home"/>
+        `;
+    }
+    else {
+        console.log("navigating to", moduleName);
+        await cachedModules[moduleName]?.default();
+        window.cacheLoads++;
+    }
+
+    replaceLinks();
+}
