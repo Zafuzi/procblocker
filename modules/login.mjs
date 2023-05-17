923476525c44832403362c7fa445508085c6d54a
Zachary Foutz <zachary@boomlearning.com>
2023-05-17 12:08:59 -0700

    * updated anchor tag styles on homepage

--- modules/login.mjs
+++ modules/login.mjs
@@ -4,7 +4,9 @@ export default function()
     rootElement.innerHTML = `
         <p>CacheLoads: ${cacheLoads}</p>
         <h2>Login</h2>
+        
         <Link href="/?page=home" data-value="Home"/>
+        
         <form action="/login" method="post">
             <label>Username:</label>
             <input type="text" name="username" />
